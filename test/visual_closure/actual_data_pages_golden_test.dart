e3635e98e1842a1e2556e7f1d3d4461a7dc2531d
bilhealth-admin <bilhealth.app@gmail.com>
2026-09-14 02:35:39 +0300

    test(golden): stabilize profile visual fixture

--- test/visual_closure/actual_data_pages_golden_test.dart
+++ test/visual_closure/actual_data_pages_golden_test.dart
@@ -140,6 +140,9 @@ void main() {
         activityLevel: 'light',
         exercises: true,
       );
+      await PreferencesRepository(
+        db,
+      ).set('timezoneName', 'Egypt Daylight Time');
     }
     return db;
   }
@@ -232,7 +235,7 @@ void main() {
             () => DateTime(2026, 8, 5, 9, 41, 12),
           ),
           premiumProfileClockProvider.overrideWithValue(
-            () => DateTime(2026, 8, 30, 9, 41, 12),
+            () => DateTime.utc(2026, 8, 30, 9, 41, 12),
           ),
           connectedHealthGatewayProvider.overrideWithValue(
             const _UnavailableHealthGateway(),
